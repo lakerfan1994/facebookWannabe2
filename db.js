a6ace95068588cea9448adeada84e6adb079f0d6
your name here <your_email_here>
2019-11-06 21:48:36 -0500

    savita's post-branch 2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const connectionString = "postgres://localhost:5432/facebook_db"
+const connectionString = "postgres://localhost:5432/fbwannabe_db"
 const db = pgp(connectionString);
 
 
