ec8d5c0fe5a9c3d3476a6a2a96a37757afcb17b4
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2019-11-06 21:41:10 -0500

    second commit to the like router

--- routes/likes.js
+++ routes/likes.js
@@ -1,15 +1,60 @@
 const express = require('express');
 let router = express.Router;
-const connectionString = {
-	host: 'localhost',
-	port: 5432, 
-	database: 'mydb',
-	user: 'Chuck',
-	password: 2494
-};
+const db= require('../db');
 
-const pgp = require('pg-promise')();
-const db = pgp(connectionString);
+router.get('/posts/:post_id', async (req, res) => {
+	let response;
+	try{
+		response = await db.any('SELECT liker_id FROM likes WHERE post_id = req.params.post_id  ');
+		res.send(response);
+	}
+	catch(err){
+		console.log('this broke :(');
+	}
+});
+
+router.post('/posts/:post_id/:commenter_id', async (req, res) => {
+	let response;
+	try{
+		response = await db.none('INSERT INTO likes(liker_id, post_id) VALUES($1, $2)', [req.body.post_id, req.body.commenter_id]);
+	}
+	catch(err){
+		console.log(err);
+	}
+
+	let messageToUser = {addedLike: req.body};
+   	res.send(messageToUser);
+})
+
+router.delete('/:post_id/:liker_id', async (req,, res) => {
+	let response;
+	try{
+		response = await db.any('DELETE FROM likes WHERE liker_id = req.body.liker_id AND post_id = req.body.post_id');
+	}
+		catch(err){
+		console.log(err);
+	}
+
+	let messageToUser = {deletedLike: req.body};
+   	res.send(messageToUser);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
