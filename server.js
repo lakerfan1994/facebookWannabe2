be17cb90044e13dfa3dd01784ef195382a5d3898
aransagarcia <aransagarcia@pursuit.org>
2019-11-07 21:04:11 -0500

    uncommented routes that are not implemeneted. Added a app.use for user Route. Added a app.listen.

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.use(express.json());
 
 // Routes
 const userRouter = require('./routes/users');
-const postRouter = require('.routes/posts');
-const commentRouter = require('./routes/comments');
-const likeRouter = require('.routes/likes')
-const albumRouter = require('./routes/albums');
-const pictureRouter = require('./routes/pictures');
+// const postRouter = require('.routes/posts');
+// const commentRouter = require('./routes/comments');
+// const likeRouter = require('.routes/likes')
+// const albumRouter = require('./routes/albums');
+// const pictureRouter = require('./routes/pictures');
+
+app.use('/users', userRouter)
+
+app.listen(port, ()=>{
+    console.log('Server is running!')
+})
