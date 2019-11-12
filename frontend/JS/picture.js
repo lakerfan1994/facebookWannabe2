11ec06c83149e17f275e9fc06ce6d51298894119
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2019-11-11 21:52:02 -0500

    small change to picture

--- frontend/JS/picture.js
+++ frontend/JS/picture.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const loadpicture = async() => {
     const container = document.querySelector('#container');
     container.innerHTML = "";
-    let albumId = localStorage.getItem()
+    let albumId = localStorage.getItem('album_id');
     const response = await axios.get(`http://localhost:3000/pictures/${albumId}`);
     for(let i = 0; i < response.data.payload.length; i++){
             let images = response.data.payload[i].picture_url
