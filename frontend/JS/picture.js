f612739e9615c6a0b78a2802d61d728c913304e2
your name here <your_email_here>
2019-11-11 21:32:52 -0500

    fixed typo

--- frontend/JS/picture.js
+++ frontend/JS/picture.js
@@ -10,7 +10,7 @@ const loadpicture = async() => {
     let albumId = localStorage.getItem()
     const response = await axios.get(`http://localhost:3000/pictures/${albumId}`);
     for(let i = 0; i < response.data.payload.length; i++){
-            let images = response.data.payload[i].picture_usrl
+            let images = response.data.payload[i].picture_url
             let img = document.createElement("img");
             img.src = images;
             container.appendChild(img);
